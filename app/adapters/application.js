4e0e005efb33e9a771b3463663d534b0795acbe8
Doug Bennett <dben89x@gmail.com>
2015-04-02 13:36:36 -0600

    changed adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,12 @@
-import DS from "ember-data";
+// import DS from "ember-data";
+//
+// export default DS.LSAdapter.extend({
+//   host: 'http://localhost:3000'
+// });
 
-export default DS.LSAdapter.extend({
-  host: 'http://localhost:3000'
+import DS from 'ember-data';
+import ENV from '../config/environment';
+
+export default DS.ActiveModelAdapter.extend({
+  host: 'https://notes-app-api.herokuapp.com'
 });
